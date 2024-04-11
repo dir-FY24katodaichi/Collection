433cfbd98f62f9571d68a95f412ebc6067015af0
dir-FY24katodaichi <daichi.kato.b566@dirbato.co.jp>
2024-04-11 09:09:52 +0900

    最初のコミット

---
+++ Collection/src/sample/ArraySample.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class ArraySample {
+
+    public static void main(String[] args) {
+        var numbers = new int[5];
+
+        for (var i = 0; i < numbers.length; i++) {	
+            numbers[i] = i * 2; // 配列の各要素に値を代入
+        
+
+    
+            System.out.println(numbers[i]);
+        }
+    }
+
+}
